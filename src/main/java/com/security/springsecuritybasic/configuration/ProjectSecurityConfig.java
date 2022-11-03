c79ddf1c7a0cd62e937f4b6bff6fbddd7af5339e
Lucero Raul <luceroraul12@gmail.com>
2022-11-03 18:25:50 -0300

    delete properties because I use accounts in memory

--- src/main/java/com/security/springsecuritybasic/configuration/ProjectSecurityConfig.java
+++ src/main/java/com/security/springsecuritybasic/configuration/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.security.springsecuritybasic.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +19,20 @@ public class ProjectSecurityConfig {
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("homito ")
+                .password("admin")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("natzka")
+                .password("user")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
