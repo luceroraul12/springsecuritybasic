0dea0d261b7394704897dbac4534ae7c2b2316a1
Lucero Raul <luceroraul12@gmail.com>
2022-11-03 11:42:39 -0300

    first commit

---
+++ src/main/java/com/security/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.security.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "In this point, you have logged in this API.";
+    }
+}
