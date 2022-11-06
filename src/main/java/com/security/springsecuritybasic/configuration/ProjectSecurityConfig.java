f1c3f6fd5a0dbe0e53419354aced70324c3d2d85
Lucero Raul <luceroraul12@gmail.com>
2022-11-06 11:19:14 -0300

    change config for InMemoryUserDetailsManager -> UserDetailesService

--- src/main/java/com/security/springsecuritybasic/configuration/ProjectSecurityConfig.java
+++ src/main/java/com/security/springsecuritybasic/configuration/ProjectSecurityConfig.java
@@ -5,10 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -22,19 +26,26 @@ public class ProjectSecurityConfig {
                 .and().httpBasic();
         return http.build();
     }
+// this I must use when I wanto to make tests without databases
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//        UserDetails admin = User.withUsername("homito")
+//                .password("admin")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User.withUsername("natzka")
+//                .password("user")
+//                .authorities("read")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
 
+//    this is when I want to use database as a user containers (all time)
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails admin = User.withUsername("homito")
-                .password("admin")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withUsername("natzka")
-                .password("user")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
