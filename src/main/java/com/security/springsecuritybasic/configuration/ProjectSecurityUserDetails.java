78179e68dd9b33688c1b81704fdd17345e89f6ee
Lucero Raul <luceroraul12@gmail.com>
2022-11-19 11:05:26 -0300

    change component -> service

--- src/main/java/com/security/springsecuritybasic/configuration/ProjectSecurityUserDetails.java
+++ src/main/java/com/security/springsecuritybasic/configuration/ProjectSecurityUserDetails.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Service
 public class ProjectSecurityUserDetails implements UserDetailsService {
 
     @Autowired
